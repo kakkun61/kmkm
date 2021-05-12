604279195d694413f732b0491007b86a16bb1b67
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2021-05-13 00:32:04 +0900

    Language.Kmkm module

--- app/kmkm.hs
+++ app/kmkm.hs
@@ -1,15 +1,14 @@
 {-# LANGUAGE DataKinds #-}
 
-import           Control.Monad.IO.Class    (MonadIO (liftIO))
-import qualified Data.List                 as L
-import qualified Data.Text.IO.Utf8         as T
-import           Language.Kmkm.Builder.C   (build)
-import           Language.Kmkm.Parser.Sexp (parse)
-import           Main.Utf8                 (withUtf8)
-import qualified Options.Declarative       as O
-import           System.Directory          (createDirectoryIfMissing)
-import           System.FilePath           (takeDirectory, (</>))
-import qualified Text.PrettyPrint          as P
+import Language.Kmkm (compile)
+
+import           Control.Monad.IO.Class (MonadIO (liftIO))
+import qualified Data.List              as L
+import qualified Data.Text.IO.Utf8      as T
+import           Main.Utf8              (withUtf8)
+import qualified Options.Declarative    as O
+import           System.Directory       (createDirectoryIfMissing)
+import           System.FilePath        (takeDirectory, (</>))
 
 main :: IO ()
 main = withUtf8 $ O.run_ main'
@@ -21,12 +20,10 @@ main'
 main' dest src =
   liftIO $ do
     srcText <- T.readFile $ O.get src
-    srcTree <- parse (O.get src) srcText
-    doc <- build srcTree
-    let destText = P.render doc
+    destText <- compile (O.get src) srcText
     case L.splitAt (length (O.get src) - 5) (O.get src) of
       (path, ".s.km") -> do
         let outputFile = O.get dest </> path ++ ".c"
         createDirectoryIfMissing True $ takeDirectory outputFile
-        writeFile outputFile destText
+        T.writeFile outputFile destText
       _  -> fail "extension is not \"s.km\""
--- src/Language/Kmkm.hs
+++ src/Language/Kmkm.hs
@@ -1,1 +1,17 @@
-module Language.Kmkm () where
+module Language.Kmkm
+  ( compile
+  ) where
+
+import Language.Kmkm.Builder.C   (build)
+import Language.Kmkm.Parser.Sexp (parse)
+
+import           Control.Monad.Catch (MonadThrow)
+import           Data.Text           (Text)
+import qualified Data.Text           as T
+import qualified Text.PrettyPrint    as P
+
+compile :: MonadThrow m => String -> Text -> m Text
+compile tag src = do
+  tree <- parse tag src
+  doc <- build tree
+  pure $ T.pack $ P.render doc
